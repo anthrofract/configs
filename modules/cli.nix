63ec262760cdd20d7f5423b19ed05b811a63f87d
Anthrofract <anthrofract@fastmail.com>
2026-04-21 02:07:36 -0500

    Manage tmux in home manager

--- modules/cli.nix
+++ modules/cli.nix
@@ -6,6 +6,8 @@ in
   flake.commonModules.cli =
     { pkgs, ... }:
     {
+      imports = [ self.commonModules.tmux ];
+
       environment.systemPackages = [
         pkgs.asciinema
         pkgs.bc
@@ -38,7 +40,6 @@ in
         pkgs.ripgrep
         pkgs.sd
         pkgs.starship
-        pkgs.tmux
         pkgs.tokei
         pkgs.tree-sitter
         pkgs.unzip
@@ -66,26 +67,18 @@ in
     };
 
   flake.nixosModules.cli =
-    { pkgs, ... }:
+    { ... }:
     {
       imports = [ self.commonModules.cli ];
-
-      # Link nushell to /usr/local/bin to make a single path for nixos and darwin
-      system.activationScripts.bin-nu.text = ''
-        mkdir -p /usr/local/bin
-        ln -sfn ${pkgs.nushell}/bin/nu /usr/local/bin/nu
-      '';
     };
 
   flake.darwinModules.cli =
-    { pkgs, ... }:
+    { ... }:
     {
       imports = [ self.commonModules.cli ];
 
-      # Link nushell to /usr/local/bin to make a single path for nixos and darwin
       # Symlink nushell config to macOS default location so config.nu loads before XDG_CONFIG_HOME is set
       system.activationScripts.postActivation.text = ''
-        sudo ln -sfn ${pkgs.nushell}/bin/nu /usr/local/bin/nu
         sudo -u ${ids.personal.userName} mkdir -p "/Users/${ids.personal.userName}/Library/Application Support/nushell"
         sudo -u ${ids.personal.userName} ln -sfn "/Users/${ids.personal.userName}/.config/nushell/config.nu" "/Users/${ids.personal.userName}/Library/Application Support/nushell/config.nu"
       '';
---
+++ modules/tmux.nix
@@ -0,0 +1,81 @@
+{ ... }:
+{
+  flake.commonModules.tmux =
+    { ... }:
+    {
+      home-manager.sharedModules = [
+        (
+          { pkgs, ... }:
+          {
+            programs.tmux = {
+              enable = true;
+              package = pkgs.tmux;
+              aggressiveResize = true;
+              baseIndex = 1;
+              escapeTime = 0;
+              focusEvents = true;
+              historyLimit = 50000;
+              keyMode = "vi";
+              mouse = true;
+              prefix = "`";
+              shell = "${pkgs.nushell}/bin/nu";
+              terminal = "xterm-ghostty";
+              extraConfig = ''
+                set-option -g display-time 4000
+                set-option -g status-interval 1
+                set-option -g detach-on-destroy off
+                set-option -g set-titles on
+                set-option -g set-titles-string "#S / #W"
+                set-option -sa terminal-overrides ",xterm-256color:Tc"
+                set-option -ga terminal-features "*:hyperlinks"
+                set-option -gq allow-passthrough on
+                set-option -g renumber-windows on
+
+                bind-key \\ split-window -h -c "#{pane_current_path}"
+                bind-key | split-window -hb -c "#{pane_current_path}"
+                bind-key - split-window -v -c "#{pane_current_path}"
+                bind-key _ split-window -vb -c "#{pane_current_path}"
+
+                bind-key Enter if-shell "[ $(tmux list-windows | wc -l) -eq 1 ]" "new-window" "last-window"
+
+                bind-key o display-popup -E "${pkgs.nushell}/bin/nu -l -c 'tms'"
+                bind-key a run-shell "${pkgs.nushell}/bin/nu -l -c 'tms ~/scratch'"
+
+                bind-key r source-file ~/.config/tmux/tmux.conf
+                bind-key Space switch-client -l
+                bind-key e select-layout -E
+                bind-key x kill-pane
+                bind-key X kill-session
+
+                bind-key -T copy-mode-vi v send -X begin-selection
+                bind-key -T copy-mode-vi V send -X select-line
+                bind-key -T copy-mode-vi WheelUpPane send-keys -X -N 2 scroll-up
+                bind-key -T copy-mode-vi WheelDownPane send-keys -X -N 2 scroll-down
+
+                set-option -g status-position bottom
+                set-option -g status-style bg="#2f312c",fg="#f1e9d2"
+
+                set-option -g window-status-style bg="#2f312c",fg="#a3a7a1"
+                set-option -g window-status-format " #{window_index}:#W#F"
+
+                set-option -g window-status-current-style bg="#2f312c",fg="#f1e9d2"
+                set-option -g window-status-current-format " (#W#{?#{==:#F,*},,#F})"
+
+                set-option -g mode-style bg="#f1e9d2",fg="#2f312c",bold
+                set-option -g message-style bg="#f1e9d2",fg="#2f312c"
+                set-option -g message-command-style bg="#f1e9d2",fg="#2f312c"
+
+                set-option -g pane-active-border-style fg="colour242"
+                set-option -g pane-border-style fg="colour242"
+
+                set-option -g status-right-length 50
+                set-option -g status-left-length 50
+                set-option -g status-left "#[bg=#2f312c,fg=#f1e9d2] [#{session_name}] "
+                set-option -g status-right "#H #{?#{==:#{pane_mode},copy-mode},#[bg=#f1e9d2]#[fg=black],}#{?client_prefix,#[bg=green]#[fg=black]  ,  }"
+              '';
+            };
+          }
+        )
+      ];
+    };
+}
